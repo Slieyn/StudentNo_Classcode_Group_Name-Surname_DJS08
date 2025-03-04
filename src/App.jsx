68fa507b7c6b277e84a4f5913c117cd2d6527045
Silindokuhle Ngubane <silindokuhlengubane0@gmail.com>
2025-03-04 21:46:00 +0200

    imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import "./server"
 
 
 function App() {
@@ -22,4 +22,5 @@ function App() {
     )
   }
 
+  
   export default App;
